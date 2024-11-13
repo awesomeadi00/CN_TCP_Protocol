a63637545a069edd26d1b7d67274e6c9117d41ec
awesomeadi00 <adityapandhare44@gmail.com>
2024-11-14 00:22:25 +0400

    Restructured makefile to be more concrete and added code where sender checks for ACKs and sends packets concurrently

--- code/obj/test.py
+++
@@ -1,5 +0,0 @@
-# write a python program that list from 1 to 1000000000 in separate lines in a file called test.txt.
-
-with open('/Users/mexiporc/Documents/GitHub/Computer-Network/Project2/starter_code/obj/test.txt', 'w') as file:
-    for i in range(1, 1000001):
-        file.write(str(i) + '\n')
